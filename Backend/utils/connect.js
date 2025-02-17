9d7fea09159827623d465a0b8b6e88c077ef81f4
Sabine <sabine.weber@dci-student.org>
2025-02-17 09:51:22 +0100

    Ordner Struktur

---
+++ Backend/utils/connect.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.MONGODB_URI);
+export async function connect() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (MONGODB_URI) {
+    await mongoose.connect(MONGODB_URI);
+    console.log(
+      ':white_check_mark: MongoDB erfolgreich verbunden :fire::fire:'
+    );
+  } else {
+    throw new Error(' :x: Fehler:  MONGODB_URL not found');
+  }
+}
