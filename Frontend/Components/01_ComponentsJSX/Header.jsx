01dc4584e97f6ac2f9fa53895769da407cc3611f
Sabine <sabine.weber@dci-student.org>
2025-03-05 13:34:35 +0100

    add Logout Button to header and navigate to login

--- Frontend/Components/01_ComponentsJSX/Header.jsx
+++ Frontend/Components/01_ComponentsJSX/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '@style/Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleIconClick = () => {
+    navigate('/'); // Navigate to the login page
+  };
+
   return (
     <div className="Header">
+      <button className="icon-button" onClick={handleIconClick}>
+        <FontAwesomeIcon icon={faRightFromBracket} />
+      </button>
       <img src="/Upload/Logo Cayenne.png" alt="logo" className="logo" />
     </div>
   );
